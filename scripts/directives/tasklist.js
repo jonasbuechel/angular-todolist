d9b725c0583c0c283eb1f505bdf0ed8e9820a14b
Jonas Büchel <jonas.buechel@gmail.com>
2016-05-10 06:09:29 +0200

    move tasklist to a directive

---
+++ scripts/directives/tasklist.js
@@ -0,0 +1,14 @@
+/*jslint browser: true, devel: true, white: true, sloppy: true*/
+/*global angular */
+
+'use strict';
+
+var todoListApp = angular.module('todoListApp');
+
+/* add directive */
+todoListApp.directive('tasklist', function() {
+    return {
+        templateUrl: 'templates/tasklist.html',
+        controller: 'todoCtrl'
+    };
+});
